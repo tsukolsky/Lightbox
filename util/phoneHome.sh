1a1e51b93c826b14b6a522eac6f33890278198fa
Todd Sukolsky <tsukolsky@gmail.com>
2014-07-17 01:56:11 +0000

    updated phone home script

--- util/phoneHome.sh
+++ util/phoneHome.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh -N -R 2222:localhost:22 tsuck@tsukolsky.dyndns.org -p 31670 &
+ssh -o "StrictHostKeyChecking no" -i /home/pi/.ssh/id_rsa -N -R 2222:localhost:22 tsuck@tsukolsky.dyndns.org -p 31670 &
